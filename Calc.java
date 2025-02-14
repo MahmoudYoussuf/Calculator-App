26703d401bb797d58f4062422204f92421461b79
Mahmoud Youssef <mahmoud20627mahmoud@gmail.com>
2025-02-14 08:59:57 +0300

    Created the frame

---
+++ Calc.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class Calc{
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Test Frame");
+        frame.setSize(500, 300);
+        frame.setVisible(true);
+    }
+}
